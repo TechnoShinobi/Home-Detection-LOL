b7cedaad4f61be7db778fb8e26fe77be51abe929
TechnoShinobi <99727350+TechnoShinobi@users.noreply.github.com>
2022-05-16 17:50:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function livingRoom(){
+window.location = "livingRoom.html"
+
+}
+
+function laptop(){
+window.location = "laptop.html"    
+
+}
+
+function miBottle(){
+window.location = "miBottle.html"
+        
+}
+
+function tv(){
+window.location = "tv.html"
+
+}
+
+function ac(){
+window.location = "ac.html"
+
+}
+
+
+
